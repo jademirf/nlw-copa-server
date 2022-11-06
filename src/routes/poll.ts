21817f229e76e83b9a5adf21679cb1bd57b8a8da
Jademir de Moura <jademirf@gmail.com>
2022-11-06 16:08:00 -0300

    add poll routes

--- src/routes/poll.ts
+++ src/routes/poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import ShortUniqueId from "short-unique-id"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 
 export async function pollRoutes(fastify: FastifyInstance) {
@@ -48,5 +49,136 @@ export async function pollRoutes(fastify: FastifyInstance) {
 
 
     return reply.status(201).send({code})
+  })
+
+  fastify.post('/polls/join', {
+    onRequest: [authenticate]
+  }, async (request, reply) => {
+    const joinPollBody = z.object({
+      code: z.string(),
+    })
+
+    const { code } = joinPollBody.parse(request.body)
+
+    const poll = prisma.poll.findUnique({
+      where: { code },
+      include: {
+        participants: {
+          where: {
+            userId: request.user.sub
+          }
+        }
+      }
+    })
+
+    if (!poll) {
+      return reply.status(400).send({
+        message: 'Poll not found'
+      })
+    }
+    if (poll.participants.length > 0) {
+      return reply.status(400).send({
+        message: 'You already joined this poll'
+      })
+    }
+    if (!poll.ownerId) {
+      await prisma.poll.update({
+        where: {
+          id: poll.id,
+        },
+        data: {
+          ownerId: request.user.sub,
+        }
+      })
+    }
+
+    await prisma.poll.create({
+      data: {
+        pollId: poll.id,
+        userId: request.user.sub,
+      }
+    })
+
+    return reply.status(201).send()
+
+  })
+
+  fastify.get('/polls', {
+    onRequest: [authenticate]
+  }, async (request) => {
+    const polls = await prisma.poll.findMany({
+      where: {
+        participants: {
+          some: {
+            userId: request.user.sub
+          }
+        }
+      },
+      include: {
+        _count: {
+          select: {
+            participants: true,
+          }
+        },
+        participants: {
+          select: {
+            id: true,
+            user: {
+              select: {
+                avatarUrl: true,
+              }
+            }
+          }, 
+          take: 4,
+        },
+        owner: {
+          select: {
+            id: true,
+            name: true,
+          }
+        }
+      }
+    })
+    return {polls}
+  })
+  fastify.get('/polls/:id', {
+    onRequest: [authenticate],
+  }, async (request) => {
+    const getPollParams = z.object({
+      id: z.string(),
+    })
+
+    const {id} = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id
+      },
+      include: {
+        _count: {
+          select: {
+            participants: true,
+          }
+        },
+        participants: {
+          select: {
+            id: true,
+            user: {
+              select: {
+                avatarUrl: true,
+              }
+            }
+          }, 
+          take: 4,
+        },
+        owner: {
+          select: {
+            id: true,
+            name: true,
+          }
+        }
+      }
+    })
+    return {poll}
   })
 }
